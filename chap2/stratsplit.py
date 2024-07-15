87e5fa3d8060330a4122cd51e65fbc827ccbbd04
midnight-koffee <debu.chatra@gmail.com>
2024-07-15 19:56:38 +0530

    finished the creating test set section

---
+++ chap2/stratsplit.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+from housing import load_housing_data
+housing = load_housing_data()
+
+housing["income_cat"] = pd.cut(housing["median_income"],
+                               bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                               labels=[1, 2, 3, 4, 5])
+housing["income_cat"].hist()
+from sklearn.model_selection import StratifiedShuffleSplit
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+
+strat_test_set["income_cat"].value_counts() / len(strat_test_set)
+
+# Remove the income_cat attribute to revert to original data
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop("income_cat", axis=1, inplace=True)
